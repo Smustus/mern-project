b7e90d4308d1cb8835deaf0f0c936044536d1017
Smustus <rikardfors_91@hotmail.com>
2025-07-20 01:12:09 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello");
+});
